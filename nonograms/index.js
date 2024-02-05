43785c7b40ac07ea042e40dcc97da932600889fc
RabbitDrew <Andrei.sp.93@gmail.com>
2024-02-05 22:59:37 +0300

    feat: add game field according the task with help of js

--- nonograms/index.js
+++ nonograms/index.js
@@ -140,7 +140,7 @@ for (let i = 0; i < 3; i++) {
   headerRow.classList.add("header-row");
   headerBody.appendChild(headerRow);
 }
-
+//
 const headerRow = document.querySelectorAll(".header-row");
 for (let i = 0; i < headerRow.length; i++) {
   for (let j = 0; j < 5; j++) {
@@ -149,7 +149,85 @@ for (let i = 0; i < headerRow.length; i++) {
     headerRow[i].appendChild(rowCell);
   }
 }
-
+//
 const rowCell = document.querySelectorAll(".row-cell");
+for (let i = 0; i < rowCell.length; i++) {
+  const cellTitle = document.createElement("h2");
+  cellTitle.classList.add("cell-title");
+  rowCell[i].appendChild(cellTitle);
+}
 
-for (let i = 0; rowCell.length; i++) {}
+const cellTitle = document.querySelectorAll(".cell-title");
+cellTitle[6].textContent = "1";
+cellTitle[8].textContent = "1";
+cellTitle[10].textContent = "5";
+cellTitle[11].textContent = "1";
+cellTitle[12].textContent = "3";
+cellTitle[13].textContent = "1";
+cellTitle[14].textContent = "3";
+
+const leftPart = document.createElement("div");
+leftPart.classList.add("left");
+gameWrapper.appendChild(leftPart);
+
+const gameAside = document.createElement("table");
+gameAside.classList.add("game-aside");
+leftPart.appendChild(gameAside);
+
+const asideBody = document.createElement("tbody");
+asideBody.classList.add("aside-body");
+gameAside.appendChild(asideBody);
+
+for (let i = 0; i < 5; i++) {
+  const asideColumn = document.createElement("tr");
+  asideColumn.classList.add("aside-column");
+  asideBody.appendChild(asideColumn);
+}
+
+const asideColumn = document.querySelectorAll(".aside-column");
+for (let i = 0; i < asideColumn.length; i++) {
+  for (let j = 0; j < 3; j++) {
+    const columnCell = document.createElement("td");
+    columnCell.classList.add("column-cell");
+    asideColumn[i].appendChild(columnCell);
+  }
+}
+
+const columnCell = document.querySelectorAll(".column-cell");
+for (let i = 0; i < columnCell.length; i++) {
+  const columnCellTitle = document.createElement("h2");
+  columnCellTitle.classList.add("cell-title__column");
+  columnCell[i].appendChild(columnCellTitle);
+}
+
+const columnCellTitle = document.querySelectorAll(".cell-title__column");
+columnCellTitle[2].textContent = "5";
+columnCellTitle[3].textContent = "1";
+columnCellTitle[4].textContent = "1";
+columnCellTitle[5].textContent = "1";
+columnCellTitle[8].textContent = "5";
+columnCellTitle[11].textContent = "1";
+columnCellTitle[14].textContent = "1";
+
+const gameField = document.createElement("table");
+gameField.classList.add("game-field");
+leftPart.appendChild(gameField);
+
+const fieldBody = document.createElement("tbody");
+fieldBody.classList.add("field-body");
+gameField.appendChild(fieldBody);
+
+for (let i = 0; i < 5; i++) {
+  const bodyRow = document.createElement("tr");
+  bodyRow.classList.add("body-row");
+  fieldBody.appendChild(bodyRow);
+}
+
+const bodyRow = document.querySelectorAll(".body-row");
+for (let i = 0; i < bodyRow.length; i++) {
+  for (let j = 0; j < 5; j++) {
+    const rowCellBody = document.createElement("td");
+    rowCellBody.classList.add("row-cell__body");
+    bodyRow[i].appendChild(rowCellBody);
+  }
+}
